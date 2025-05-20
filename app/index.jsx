b9713738b1c36f18ec3f27bc6e627ffaa933c056
Bibek Dhimal <bbekdml2020@gmail.com>
2025-05-20 20:13:03 +0545

    Lesson 2 commited

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from "react-native";
+import Logo from "../assets/img/React_Native_logo.png";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Hello This is a Home Page!</Text>
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://www.onu.ro/wp/wp-content/uploads/2020/03/react-native-logo-768x890.png",
+        }}
+        style={styles.logo}
+      />
+      <Text style={styles.title}>The number 1</Text>
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+
+      <View style={styles.card}>
+        <Text>Hello, This is a card.</Text>
+      </View>
+      <Image
+        source={Logo}
+        style={{
+          width: 200,
+          height: 200,
+          resizeMode: "contain",
+          marginTop: 20,
+        }}
+      />
     </View>
-  )
-}
+  );
+};
 
 export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  card: {
+    backgroundColor: "#eee",
+    padding: 20,
+    borderRadius: 5,
+    boxShadow: "4px 4px rgba(0, 0, 0, 0.1)",
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    resizeMode: "contain",
+    marginBottom: 20,
+  },
+});
