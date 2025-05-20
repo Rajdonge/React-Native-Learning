2cc35b78f28239210c78ed0f77d9d37771f2b9a3
Bibek Dhimal <bbekdml2020@gmail.com>
2025-05-20 11:20:34 +0545

    Lesson 1 commited

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Hello This is a Home Page!</Text>
+    </View>
+  )
+}
+
+export default Home;
+
+const styles = StyleSheet.create({})
